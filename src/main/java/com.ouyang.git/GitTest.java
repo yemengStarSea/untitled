ec3c2525d50de602f08bfe40a82c9049be15c0e2
夜梦StarSea <110138449+yemengStarSea@users.noreply.github.com>
2022-08-03 07:57:38 +0800

    pull commit

--- src/main/java/com.ouyang.git/GitTest.java
+++ src/main/java/com.ouyang.git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
