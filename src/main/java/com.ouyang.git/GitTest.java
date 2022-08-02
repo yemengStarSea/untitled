cc3a53a02d463416151e9415b6e1955855d0c9dc
Layne <Layne@ouyang.com>
2022-08-03 06:39:51 +0800

    second commit

--- src/main/java/com.ouyang.git/GitTest.java
+++ src/main/java/com.ouyang.git/GitTest.java
@@ -3,5 +3,6 @@ package main.java.com.ouyang.git;
 public class GitTest {
     public static  void main(String[] args){
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
